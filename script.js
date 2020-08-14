fb1deeb73288af451eb0ab56dd3470421e613f6e
Alicia Morley <aliciaseal81@gmail.com>
2020-08-14 16:20:28 -0700

    work in progress

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-// $('.carousel').carousel({
-//     interval: 5000
-//   })
+$(document).ready(function () {
+
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        //>=, not <=
+        if (scroll >= 60) {
+            //clearHeader, not clearheader - caps H
+            $(".navbar").addClass("nav-color-change");
+            $(".nav-link").addClass("nav-link-white");
+            $(".hero-text").addClass("nav-link-white");
+        } else {
+            $(".navbar").removeClass("nav-color-change");
+            $(".nav-link").removeClass("nav-link-white");
+            $(".hero-text").removeClass("nav-link-white");
+        }
+    }); //missing );
+
+
+
+
+
+
+    // document ready  
+});
