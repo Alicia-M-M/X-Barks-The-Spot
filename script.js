8a59a5c2396556af042bfac93acd9bb26728ccb1
Alicia Morley <aliciaseal81@gmail.com>
2020-08-17 19:15:37 -0700

    work in progress v2 navbar

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
+// // full screen tellow backgroud on scroll:
+//     $(window).scroll(function () {
+//         var scroll = $(window).scrollTop();
+
+//         //>=, not <=
+//         if (scroll >= 60 && window.innerWidth > 575) {
+//             //clearHeader, not clearheader - caps H
+//             $(".navbar").addClass("nav-color-change");
+
+//             // $(".hero-text").hide();
+//         } else {
+//             $(".navbar").removeClass("nav-color-change");
+//             // $(".hero-text").show();
+
+//         }
+//     }); //missing );
+
+
+// // side-nav-close button
+
+
+
+//     // document ready  
+// });
+
+
+$(document).ready(function () {
 
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
 
-        //>=, not <=
-        if (scroll >= 60) {
-            //clearHeader, not clearheader - caps H
-            $(".navbar").addClass("nav-color-change");
-            // $(".hero-text").hide();
+        if (scroll >= 60 && window.innerWidth > 575) {
+            $(".navbar").addClass("nav-color-change ul");
         } else {
-            $(".navbar").removeClass("nav-color-change");
-            // $(".hero-text").show();
-        }
-    }); //missing );
+            $(".navbar").removeClass("nav-color-change ul");
+        };
 
+        if (scroll >= 20 && window.innerWidth <= 575) {
+            $(".navbar").addClass("navbar-mobile");
+        } else {
+            $(".navbar").removeClass("navbar-mobile");
+        };
 
-
-
-
-
-    // document ready  
+    });
 });
+
+
 
