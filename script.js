a235e267f5f52941af26734ea95eb091046612e7
Alicia Morley <aliciaseal81@gmail.com>
2020-08-20 19:45:02 -0700

    modal welcome-box

--- script.js
+++ script.js
@@ -17,7 +17,43 @@ $(document).ready(function () {
         };
 
     });
-    
+
+    function parallax(element, distance, speed) {
+        const item = document.querySelector(element);
+        item.style.transform = `translateY(${distance * speed}px)`;
+    }
+
+    window.addEventListener('scroll', function () {
+
+
+        parallax('.tea-cup', window.scrollY, 0.1);
+        parallax('.bone-singular-3', window.scrollY, 0.1);
+
+    });
+
+    // const imageDisplay = document.getElementByClass("card-header");
+    // imageDisplay.addEventListener('onclick', showImage);
+
+    // function showImage() {
+    //     if (imageDisplay === "none") {
+    //         imageDisplay.style.display = "block"
+    //     }else{
+    //         document.getElementByClass("clickimage").style.display = "none"
+    //     }
+
+    // };
+
+    // $(function() {
+    //     $(".card-header").click(function(){
+    //         $(this).addClass(".card-head2");
+    //         $(this).removeClass(".card-head");
+    //     });
+    // });
+
+    // $('.collapse').on(shown.bs.collapse, function(){
+    //     $(".btn-link").click(function(){
+    //     $(".card-header").collapse("show");
+    // })
 });
 
 
